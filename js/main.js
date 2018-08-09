d048d3e87b4bc25cd32765aeb7bacbd0f2f1a06d
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-08-08 23:29:39 -0700

    Add setTimeout to watch.

--- js/main.js
+++ js/main.js
@@ -2,8 +2,22 @@ new Vue({
 	el: '#app',
   data: {
 		counter: 0,
+		secondCounter: 0,
 		result: ''
   },
+	computed: {
+		output: function() {
+			return this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
+		}
+	},
+	watch: {
+		counter: function(value) {
+			let that = this;
+			setTimeout(function() {
+				that.counter = 0;
+			}, 2000);
+		}
+	},
 	methods: {
 		resultRender: function() {
 			return this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
