d535c119b6610783c6a464c6b0ea56b02d76561d
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-08-08 22:10:10 -0700

    Add increase, decrease, and renderResult methods.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 new Vue({
 	el: '#app',
   data: {
-		name: 'Max'
-  }
+		counter: 0,
+		result: ''
+  },
+	methods: {
+		resultRender: function() {
+			this.result = this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
+		},
+		increase: function() {
+			this.counter++;
+			this.resultRender();
+		},
+		decrease: function() {
+			this.counter--;
+			this.resultRender();
+		}
+	}
 });
